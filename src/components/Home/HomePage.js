f59b53ec89622169a8cf19a03c925e7f1db3cbcf
mikel-js <michaelcastro.js@gmail.com>
2020-08-29 22:59:15 +0300

    style: added loading gif

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StyledHeader from '../../elements/StyledHeader';
 import PackageTable from './PackageTable';
 import styled from 'styled-components';
+import logo from '../../images/potential.gif';
 
 const HomeWrapper = styled.div`
   background: linear-gradient(141deg, #7d8581 0%, #4d4855 51%, #57606f 75%);
 `;
 
-const HomePage = ({ packages }) => {
+const LogoWrapper = styled.div`
+  padding-top: 20vh;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+`;
+
+const LoadingLogo = styled.img`
+  width: 50vw,
+  height: 50vh
+`;
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
     <HomeWrapper>
-      <StyledHeader headerText='Home Page' />
-      <PackageTable />
+      {isLoading ? (
+        <LogoWrapper>
+          <h1>REAKTOR LOADING</h1>
+          <LoadingLogo src={logo} />
+        </LogoWrapper>
+      ) : (
+        <>
+          <StyledHeader headerText='Home Page' />
+          <PackageTable />
+        </>
+      )}
     </HomeWrapper>
   );
 };
