9ff5fd583b4be2b57a2e18ca12ae2affa2c66511
mikel-js <michaelcastro.js@gmail.com>
2020-08-30 23:39:29 +0300

    style: fix loading gif and edited README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Reaktor
+# Reaktor Software Developer, fall 2020
+
+<h4>About the App</h4>
+<p>The app is the pre-assignment of Reaktor, junior developer position. It shows the content of /var/lib/dpkg/status on Debian and Ubuntu system.
+The landing page is a simple page with button to get to the homepage. In the homepage are the package names. The package names are also links to their own page. In their own page, you can find the description of the package, and their dependencies. You can click the dependencies to show a modal that list the reverse dependencies, or the packages that depends on the clicked dependency. Clicking the package in the modal will redirect you to the package page.  
+</p>
+
+<h4>Technologies used</h4>
+I used React and Hooks in the Front end and NodeJS at the backend. I limit the use of external dependencies, as noted in the instruction. I deploy the app at Heroku https://reaktor-junior-michael.herokuapp.com/. I also noticed you are using some cloud services so I studied AWS and deployed my app at the last minute. You can find it at http://ec2-100-24-205-61.compute-1.amazonaws.com/. 
+<ul>
+  <li>React</li>
+  <li>Hooks</li>
+  <li>NodeJS</li>
+  <li>React-Bootstrap</li>
+  <li>GitHub</li>
+  <li>Heroku</li>
+  <li>AWS EC2</li>
+</ul>
+
+<h4>Links to the demo site</h4>
+<p>AWS: http://ec2-100-24-205-61.compute-1.amazonaws.com/</p>
+<p>Heroku: https://reaktor-junior-michael.herokuapp.com/</p>
--- src/components/Home/PackageTable.js
+++ src/components/Home/PackageTable.js
@@ -3,22 +3,44 @@ import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Packages from '../../constants/packages';
 import styled from 'styled-components';
+import logo from '../../images/potential.gif';
 
 const PackageLink = styled.p`
   &:hover {
     color: white;
   }
 `;
+
+const LogoWrapper = styled.div`
+  position: absolute;
+  padding-top: 10vh;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+  top: 0;
+  background: linear-gradient(141deg, #7d8581 0%, #4d4855 51%, #57606f 75%);
+`;
+const LoadingLogo = styled.img`
+  width: 50vw;
+  height: 50vh;
+`;
+
 const PackageTable = () => {
   const [packages, setPackages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     Packages()
-      .then((data) =>
-        data.sort((a, b) => {
+      .then((data) => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+        return data.sort((a, b) => {
           return a.Package > b.Package ? 1 : -1;
-        })
-      )
-      .then((sortedData) => setPackages(sortedData))
+        });
+      })
+      .then((sortedData) => {
+        setPackages(sortedData);
+      })
       .catch((err) => {
         console.error(`error at PackageTable: ${err}`);
       });
@@ -36,33 +58,42 @@ const PackageTable = () => {
   };
 
   return (
-    <Table responsive='xl' style={tableStyle}>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Package Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {packages.map((bundle, index) => (
-          <tr key={index}>
-            <td>{index + 1}.</td>
-            <td>
-              {' '}
-              <Link
-                to={`/${bundle.Package}`}
-                style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}
-              >
-                <PackageLink> {bundle.Package}</PackageLink>
-              </Link>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <>
+      {isLoading ? (
+        <LogoWrapper>
+          <h1>REAKTOR LOADING</h1>
+          <LoadingLogo src={logo} />
+        </LogoWrapper>
+      ) : (
+        <Table responsive='xl' style={tableStyle}>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Package Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {packages.map((bundle, index) => (
+              <tr key={index}>
+                <td>{index + 1}.</td>
+                <td>
+                  {' '}
+                  <Link
+                    to={`/${bundle.Package}`}
+                    style={{
+                      textDecoration: 'none',
+                      color: 'black',
+                    }}
+                  >
+                    <PackageLink> {bundle.Package}</PackageLink>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
   );
 };
 
